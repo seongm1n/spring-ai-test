12e72d44dcad58cb1c0b559b0593af1bdaeaafa2
seongm1n <0411tjdals34@gmail.com>
2025-04-01 22:59:31 +0900

    feat: defaultSystem 값 입력

--- src/main/java/hello/spring_ai/chat/ChatService.java
+++ src/main/java/hello/spring_ai/chat/ChatService.java
@@ -6,15 +6,27 @@ import org.springframework.stereotype.Service;
 @Service
 public class ChatService {
 
-    private final ChatClient.Builder builder;
+    private final ChatClient chatClient;
 
     public ChatService(ChatClient.Builder builder) {
-        this.builder = builder;
+        this.chatClient = builder
+                .defaultSystem("""
+                        내 이름은 테오야.
+                        앞으로 모든 답변은 한국어로 해줘.
+                        """)
+                .build();
     }
 
     public ChatResponse chat(ChatRequest request) {
-        ChatClient chatClient = builder.build();
-        String content = chatClient.prompt(request.message()).call().content();
-        return new ChatResponse(content);
+        try {
+            String content = chatClient
+                    .prompt(request.message())
+                    .call()
+                    .content();
+
+            return new ChatResponse(content);
+        } catch (Exception e) {
+            return new ChatResponse("AI 응답을 생성하는 중 오류가 발생했습니다.");
+        }
     }
 }
